c259e06a58a44a3547f0fad1e4cdabfb78aa0e72
adanlessossi <adanlessossi@hotmail.com>
2020-09-25 15:50:53 +0200

    Updated the application

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hello-world-java-rest
+
+http://localhost:8080/api/hello
+
+
--- src/main/java/com/example/java/rest/controller/HelloWorldJavaRestController.java
+++ src/main/java/com/example/java/rest/controller/HelloWorldJavaRestController.java
@@ -1,12 +1,14 @@
 package com.example.java.rest.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class HelloWorldJavaRestController {
 
-    @GetMapping(path = "/")
+    @GetMapping(path = "/hello")
     public String helloWorld() {
         return "{\"message\":\"Hello World Java v1\"}";
     }
